97164a98a1f29f7f75eeb3874cf801645e70c3ca
Jeane Philip Zamora <jeanozamora@gmail.com>
2026-05-22 06:35:14 +0800

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between py-4">
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <Image
